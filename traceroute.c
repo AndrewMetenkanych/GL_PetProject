8b46f255a8aceea2685fcc97ef7ee3c95bb40ebf
TheBearJew <andrew.metenkanych@mail.com>
2025-03-06 16:58:52 +0000

    Implement Packet Sending

--- traceroute.c
+++ traceroute.c
@@ -10,6 +10,34 @@
 
 #define PACKET_SIZE 64
 
+
+struct icmp_packet {
+    struct icmphdr hdr;
+    char msg[PACKET_SIZE - sizeof(struct icmphdr)];
+};
+
+// Function to create an ICMP Echo Request
+void create_icmp_packet(struct icmp_packet *pkt, int seq) {
+    memset(pkt, 0, sizeof(struct icmp_packet));
+    pkt->hdr.type = ICMP_ECHO;
+    pkt->hdr.code = 0;
+    pkt->hdr.un.echo.id = getpid();
+    pkt->hdr.un.echo.sequence = seq;
+    pkt->hdr.checksum = 0;  // Checksum will be calculated later
+}
+
+// Function to send the ICMP packet
+int send_icmp(int sockfd, struct sockaddr_in *dest, int ttl) {
+    struct icmp_packet pkt;
+    create_icmp_packet(&pkt, ttl);
+
+    // Set TTL
+    setsockopt(sockfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));
+
+    // Send packet
+    return sendto(sockfd, &pkt, sizeof(pkt), 0, (struct sockaddr*)dest, sizeof(*dest));
+}
+
 int main() {
     int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
     if (sockfd < 0) {
